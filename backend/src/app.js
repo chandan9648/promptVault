6568e62ba1ab9c00fc9b968ffa9797df087d065d
Chandan Chaudhary <chandankkumar156@gmail.com>
2025-11-05 00:03:15 +0530

    Update CORS middleware to specify allowed HTTP methods

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ 
   origin: ["https://promptvaultcloud.vercel.app", "http://localhost:5173"],
-    credentials: true 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
 }));
 app.use(morgan('dev'));
 app.use(helmet());
